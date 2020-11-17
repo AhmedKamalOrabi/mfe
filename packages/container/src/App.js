a30092e53155fd8d7945c16139b37b65633356af
AhmedRagabKamal <ahmed.ragab.kamal@gmail.com>
2020-11-17 22:30:23 +0200

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+    return (
+        <Fragment>
+            <h1>Container</h1>
+            <hr />
+            <MarketingApp />
+        </Fragment>
+    )
+}
