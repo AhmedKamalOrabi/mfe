2d1fcd7bdda7f558403cd64dc48794b76b4a359d
AhmedRagabKamal <ahmed.ragab.kamal@gmail.com>
2020-11-19 16:09:24 +0200

    Update container with proper workflow github actions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,22 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 
 export default () => {
-    return (
-        <Fragment>
-            <h1>Container</h1>
-            <hr />
-            <MarketingApp />
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
+      </BrowserRouter>
+    </Fragment>
+  )
 }
